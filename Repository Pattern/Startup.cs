e102e52541fa71790c93a198dbbb7f8c3bb7ae27
Wang Ming-Hsi <karta571110@gmail.com>
2021-01-31 01:37:22 +0800

    132

--- Repository Pattern/Startup.cs
+++ Repository Pattern/Startup.cs
@@ -23,6 +23,8 @@ namespace Repository_Pattern
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<TodoContext>(opt =>
+                   opt.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
             services.AddControllersWithViews();
         }
 
---
+++ Service/RPDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace Service
+{
+    public class RPDbContext:DbContext
+    {
+        public RPDbContext(DbContextOptions<RPDbContext> options):base(options)
+        {
+
+        }
+    }
+}
